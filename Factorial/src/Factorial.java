c3a52800a399b2d0cd9af6f69b31113b087388b2
Erman Louie Faminiano <119798034+itsErman@users.noreply.github.com>
2023-01-06 19:51:48 +0800

    Factorial using java
    This is a console based  java program.

---
+++ Factorial/src/Factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int number;
+		int factorial = 1;
+		System.out.print("Enter a number: ");
+		number = scan.nextInt();
+		
+		for(int x = 1; x<= number; x++) {
+			System.out.print(x + "*" +factorial + " = ");
+			factorial = factorial * x;
+			System.out.println(factorial);
+		}
+		System.out.println("The answer: " + factorial);
+	}
+
+}
